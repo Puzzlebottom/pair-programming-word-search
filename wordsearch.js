d9aeaf09b3d709950536633762eb1395ec836329
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-05 14:52:10 -0400

    completed in liveshare with @hissak

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,65 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const wordSearch = function(puzzle, word) {
+  if (!puzzle || !word) throw new Error('No args!');
+  if (!Array.isArray(puzzle)) throw new Error('Wrong args!');
+  if (typeof word !== "string") throw new Error('Wrong args!');
+
+  word = word.toUpperCase();
+  const reversedWord = word.split('').reverse().join('');
+  const rows = puzzle.map(row => row.join('').toUpperCase());
+  const columns = [];
+  const diagonals = [];
+  for (let i = 0; i < puzzle[0].length; i++) {
+    let column = [];
+    for (let row of puzzle) {
+      column.push(row[i]);
+    }
+    columns.push(column.join("").toUpperCase());
+  }
+
+  // for (let i = 0; i < puzzle[0].length; i++) {
+  // for (let x = 0; x < puzzle[0].length; x++) {
+  //   for(let row of puzzle) {
+  //     let diagonal = [];
+  //     diagonal.push(row[x+i])
+  //     diagonals.push(diagonal.join(""))
+  //   console.log('diagonal', diagonal)
+  //   console.log('diagonals'. diagonals)
+  //   };
+  // }}
+
+  for (const row of rows) {
+
+    if (row.includes(word) || row.includes(reversedWord)) {
+      return true;
+    }
+  }
+
+  for (const column of columns) {
+    if (column.includes(word) || column.includes(reversedWord)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// wordSearch([
+//   ['A', 'W', 'C', ],
+//   ['S', 'E', 'I', ],
+//   ['Y', 'F', 'C', ],
+// ], 'FRANK')
+
+module.exports = wordSearch;
+
+// => ['A', 'SW', 'YEC', 'FI', 'C', 'Y', 'SF', 'AEC', 'WI', 'C']
+// => RC
+// => [[00], [10, 01], [20, 11, 02], [21, 12], [02], [20], ...]
+// ==> ]
+
+// => returnRow()
+// => returnColumn()
+
+//STRETCH
+// => returnDiagonal()
+// => string.split("").reverse().join("")
